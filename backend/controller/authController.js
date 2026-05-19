46d47538d861ce1e9f65923859a5122089a71b66
Its33shubh <shubhamkakloter123@gmail.com>
2026-05-19 17:49:40 +0530

    add the user model,controller

---
+++ backend/controller/authController.js
@@ -0,0 +1,71 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+
+ const registerUser = async(req,res)=>{
+    try {
+        let {username,email,phone,password} = req.body
+
+        if(!username || !email || !phone || !password) {
+           return res.status(400).json({
+                error:true,
+                success:false,
+                message:"all filed are required"
+            })
+        }
+
+        let emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        if (!emailReg.test(email)) {
+            return res.status(400).json({
+                error:true,
+                success:false,
+                message: "Enter valid email please"
+            })
+        }
+
+        let userExist = await User.findOne({email})
+
+        if(userExist)
+        {
+            return res.status(409).json({
+                error:true,
+                success:false,
+                message: "user already exist"
+            })
+        }
+
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+        username,
+        email,
+        phone,
+        password:hashedPassword
+    })
+        return res.status(201).json({
+            error:false,
+            success:true,
+            message: "user register successfully",
+            data:{
+                id:newUser.id,
+                username:newUser.username,
+                email:newUser.email,
+                phone:newUser.phone
+            }
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            error:true,
+            success:false,
+            message:error.message
+        })
+    }
+}   
+
+module.exports = {
+    registerUser
+}
