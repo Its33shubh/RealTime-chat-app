acaf0541ad88a9ffc383f0181e9bfd8be6708e7e
Its33shubh <shubhamkakloter123@gmail.com>
2026-05-19 18:13:26 +0530

    add the api for login  the user

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -6,8 +6,7 @@ const bcrypt = require('bcryptjs')
  const registerUser = async(req,res)=>{
     try {
         let {username,email,phone,password} = req.body
-        console.log(username,email,phone,password);
-        
+       
 
         if(!username || !email || !phone || !password) {
            return res.status(400).json({
@@ -66,8 +65,80 @@ const bcrypt = require('bcryptjs')
             message:error.message
         })
     }
-}   
+} 
+
+const loginUser = async (req,res) => {
+    try {
+        let {email,password} = req.body
+
+        if(!email || !password) {
+            return res.status(400).json({
+                 error:true,
+                 success:false,
+                 message:"all filed are required"
+             })
+         }
+         let emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        if (!emailReg.test(email)) {
+            return res.status(400).json({
+                error:true,
+                success:false,
+                message: "Enter valid email please"
+            })
+        }
+
+        let userExist = await User.findOne({email})
+
+        if(!userExist)
+        {
+            return res.status(409).json({
+                error:true,
+                success:false,
+                message: "user not found"
+            })
+        }
+
+        let checkPassword = await bcrypt.compare(password,userExist.password)
+        
+        if (!checkPassword) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "invalid password"
+            })
+        }
+
+        let token = jwt.sign(
+            {id:userExist.id},
+            process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        )
+
+        return res.status(200).json({
+            error: false,
+            success: true,
+            message: "User login successful",
+            token,
+            user: {
+                id: userExist._id,
+                username: userExist.username,
+                email: userExist.email,
+                phone: userExist.phone
+            }
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            error:true,
+            success:false,
+            message:error.message
+        })
+    }
+
+}
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 
 const express = require("express");
 
-const {registerUser} = require('../controller/authController')
+const {registerUser,loginUser} = require('../controller/authController')
 
 const router = express.Router()
 
 router.post('/register',registerUser)
+router.post('/login',loginUser)
 
 module.exports = router
